0d8ad78d2a9be9d8ba541cb56e376c54ad1e9d6e
ujk-droid <03312436713aa@gmail.com>
2024-04-25 11:04:26 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bgGreen("==========*wellcome to the game*=========="));
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "please gusse a number 1 to 10 ",
+    },
+]);
+console.log(chalk.bgGreen("====================***===================="));
+if (answer.userGuessNumber === randomNumber) {
+    console.log(chalk.bgGreen("====================congratulation you guess right nmber"));
+}
+else {
+    console.log(chalk.bgGreen("sorry you guess wrong number"));
+}
